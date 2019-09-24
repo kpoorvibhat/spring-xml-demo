e01f77bb0441914b31f3c93f0f35615d0069c821
PoorviBhat <kpoorvibhat@gmail.com>
2019-09-24 23:17:52 +0530

    secondtask

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -6,20 +6,33 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
-        ClassPathResource res = new ClassPathResource("beans");
-        XmlBeanFactory factory = new XmlBeanFactory(res);
+        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans"));
         System.out.println("Beans Loaded Using XmlBeanFactory");
-        Movie movie1 = factory.getBean("movie", Movie.class);
+        Movie movie1 = factory.getBean("movie1", Movie.class);
+        Movie movie4 = factory.getBean("movie4", Movie.class);
         movie1.movieInfo();
+        System.out.println(movie1==movie4);
         System.out.println();
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans");
         System.out.println("Beans Loaded Using ApplicationContext");
-        Movie movie = context.getBean("movie", Movie.class);
-        movie.movieInfo();
+        Movie movie2 = context.getBean("movie2", Movie.class);
+        Movie movieCompare = context.getBean("movie2", Movie.class);
+
+        Movie movie3 = context.getBean("movie3", Movie.class);
+        Movie movie5 = context.getBean("movie3", Movie.class);
+
+        movie2.movieInfo();
+        movie3.movieInfo();
+        //prototype returns false
+        System.out.println(movie2==movieCompare);
+        //singleton returns true
+        System.out.println(movie3==movie5);
         System.out.println();
 
     }

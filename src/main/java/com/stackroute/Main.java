fe3531011b35b23c44adbf3887723504ea4a79c0
PoorviBhat <kpoorvibhat@gmail.com>
2019-09-25 08:53:07 +0530

    task4

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,8 +1,10 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +14,14 @@ public class Main {
         Movie movie1 = context.getBean("movie1", Movie.class);
         movie1.movieInfo();
         System.out.println();
+
+        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans"));
+        Movie movie2 = context.getBean("movie2", Movie.class);
+        movie2.movieInfo();
+
+        movie1.setApplicationContext(context);
+        movie2.setBeanFactory(factory);
+        movie1.setBeanName("FirstMovie Bean");
 
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,10 +1,18 @@
 package com.stackroute.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
-    Actor actor;
+    private Actor actor;
+    private ApplicationContextAware applicationContextAware;
+    private BeanFactoryAware beanFactoryAware;
+    private BeanNameAware beanNameAware;
 
     public Movie(Actor actor) {
         this.actor = actor;
@@ -21,6 +29,18 @@ public class Movie {
     public void movieInfo(){
         System.out.println("Movie Info");
         actor.actorInfo();
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory);
+    }
+
+    public void setBeanName(String s) {
+        System.out.println(s);
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext);
     }
 
 }
